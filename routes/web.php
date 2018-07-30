d91dd71840b3ac6cb1abd9df03e20f5c50460511
0615-3708-cect@cec.edu.ph <0615-3708-cect@cec.edu.ph>
2018-07-30 15:42:57 +0800

    controller middleware process

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,71 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('new-page', function () {
+ $names = ["arrah", "nieva", "serohijos"];
+
+    return view('new-page', array('names'=>$names));
+})->name('new-page');
+
+Route::get('new-page1', function () {
+    return view('new-page1');
+})->name('new-page1');
+
+Route::get('new-page2', function () {
+    return view('new-page2');
+})->name('new-page2');
+
+Route::get('/shoutout/{text}', function ($text) {
+    return view('shoutout', array('text' => $text));
+})->name('shoutout');
+
+
+Route::get('/foo', function () {
+    return view('foo');
+})->name('foo');
+
+Route::get('/bar', function () {
+    return view('bar');
+})->name('bar');
+
+
+Route::get('/shoutout/{text}', function ($text) {
+	$colors = array(
+				"red" => "for passion",
+				"green" => "color of nature",
+				"blue" => "It is the color of the sky",
+				"white" => "pureness",
+				"black" => "being bold",
+				"silver" => "authentic",
+				"yellow" => "warmth"
+			);
+    return view('shoutout',
+				array(
+					
+					'text' => $text,
+					'colors' => $colors
+				));
+})->name('shoutout');
+
+
+Route::get('/basic-arithmetic/{operator?}/{n1?}/{n2?}', function($operator=null,$n1=null,$n2=null){
+	
+
+	return view('/basic-arithmetic',
+
+					array(
+
+							'n1' => $n1,
+							'n2' => $n2,
+							'operator' => $operator,
+
+						 )
+			  );
+
+
+	
+})->name('basic-arithmetic');
+
+Route::get('/middleware/{age}', function ($age) {
+    return view('age', array('age'=>$age));
+})->middleware('age');
